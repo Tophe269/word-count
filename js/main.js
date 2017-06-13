d823d5ab39b3cf14ab8b5dcb97200cf7f92a6d68
Tophe269 <christophe.lachuer@gmail.com>
2017-06-13 13:45:19 +0200

    type check fix on js file

--- js/main.js
+++ js/main.js
@@ -1,25 +1,34 @@
 var app = {
   countWords: function countWords (text) {
     var wordList = Object.create(null)
+
+    if (typeof text !== 'string') { return wordList }
+
     text.split(/[\s.,?!;:'"()]+/).forEach(function countWord (word) {
-  	  var tempWord = word.toLowerCase()
+      var tempWord = word.toLowerCase()
       if (wordList[tempWord]) {
         wordList[tempWord] += 1
-      }
-      else if (tempWord.length >= 2) {
+      } else if (tempWord.length >= 2) {
         wordList[tempWord] = 1
       }
     })
+
     return wordList
   },
+
   createUlFromWordsList: function createUlFromWordsList (list) {
-  	var myUl = document.createElement("ul")
-  	Object.keys(list).forEach(function (index) {
-      var li = document.createElement("li"),
-        text = document.createTextNode(index + ' : ' + list[index])
+    var myUl = document.createElement('ul')
+
+    if (typeof list !== 'object') { return myUl }
+
+    Object.keys(list).sort().forEach(function (index) {
+      var li = document.createElement('li')
+      var text = document.createTextNode(index + ' : ' + list[index])
+
       li.appendChild(text)
       myUl.appendChild(li)
     })
+
     return myUl
   }
 }
@@ -31,4 +40,3 @@ document.body.appendChild(
     )
   )
 )
-
